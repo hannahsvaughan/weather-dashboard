4e8ff17f0fb90824aec0d9ba57ff6ff5cb6db39b
hannahsvaughan <hannahvaughan421@gmail.com>
2023-01-09 16:16:55 -0500

    debugged HTML and JS

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var APIKey = "25ee199cf6b912ff000a1bab44ef907b";
+const APIKey = "25ee199cf6b912ff000a1bab44ef907b";
 var cities = [];
 cities.reverse();
 
@@ -13,6 +13,7 @@ function loadFromStorage(){
 function saveToStorage() {
     localStorage.setItem("cities", JSON.stringify(cities));
 }
+
 
 $(document).ready(function () {
     loadFromStorage();
@@ -41,7 +42,7 @@ $(document).ready(function () {
 //get city weather info
 function getCity(city) {
     var currentDate = moment().format("LL");
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
 
 // get API data with Ajax
@@ -49,7 +50,7 @@ function getCity(city) {
         console.log(response);
 
         var iconLoc = response.weather[0].icon;
-        var iconSrc = "https://openweathermap.org/img/w/" + iconLoc + ".png";
+        var iconSrc = "https://openweathermap.org/img/wn/" + iconLoc + "@2x.png";
         var iconImage = $("<img>");
         iconImage.attr("src", iconSrc);
 
@@ -62,9 +63,9 @@ function getCity(city) {
         // TO DO - Need to convert the temperature but idk how
         var tempFahr = (response.main.temp - 273.15) * 1.8 + 32;
         $(".tempFahr").text("Temperature (Kelvin) " + tempFahr);
-        getUV(response.coord.lat, response.coord.lon)
+        getUV(response.coord.lat, response.coord.lon);
         forecast(city);
-        input.val("");
+        // input.val("");
         });
 }
 
@@ -79,17 +80,17 @@ function citiesDisplay() {
         limit = 10;
     }
 
-    $("#cityViewed").html("");
+    $("#cityView").html("");
     for (var i = 0; i < limit; i++) {
-        var cityViewed = $("<div>");
-        cityViewed.addClass("row").css({
+        var cityView = $("<div>");
+        cityView.addClass("row").css({
             textAlign: "center",
             border: "1px solid white",
             height: "50px",
             lineHeight: "50px",
             paddingLeft: "40px",
         });
-        cityViewed.html(cities[i]);
+        cityView.html(cities[i]);
         $(`#${cities[i]}`).on("click", function () {
             getCity($(this).text());
         });
@@ -117,7 +118,7 @@ function forecast(city) {
         $("#forecast").html("");
         for (var i = 39; i >= 0; i = i - 8) {
             var temp = ((list[i].main.temp - 273.15) * 1.8 + 32).toFixed(2);
-            var iconID = list[i].waether[0].icon;
+            var iconID = list[i].weather[0].icon;
             var humidity = list[i].main.humidity;
             var date = new Date(list[i].dt_txt);
 
@@ -132,16 +133,18 @@ function forecast(city) {
             col.addClass("col");
             var myCard = $("<div>");
             myCard.addClass("card");
-            closed.append(myCard);
+            col.append(myCard);
 
             // creating a p tag with response item
             var p = $("<p>").text(newDate);
 
             // creating and storing an image tag
-            var iconURL = "https://openweathermap.org/img/wn" + iconID + "@2x.png";
+            var iconURL = "https://openweathermap.org/img/w/" + iconID + ".png";
 
             var weatherImage = $("<img>");
             weatherImage.attr("src", iconURL);
+
+
 
             var p1 = $("<p>").text("Temp: " + temp + "F");
             var p2 = $("<p>").text("Humidity: " + humidity + "%");
@@ -154,7 +157,6 @@ function forecast(city) {
             myCard.append(p2);
 
             $("#forecast").prepend(col);
-
         }
     });
 }
