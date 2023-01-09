e4776b37e707632e31168d59e7db991e8964a9da
hannahsvaughan <hannahvaughan421@gmail.com>
2023-01-09 15:44:29 -0500

    continuing work on JS

---
+++ assets/script.js
@@ -0,0 +1,167 @@
+var APIKey = "25ee199cf6b912ff000a1bab44ef907b";
+var cities = [];
+cities.reverse();
+
+// api.openweathermap.org/data/2.5/weather?q={city}&appid={25ee199cf6b912ff000a1bab44ef907b}
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+function loadFromStorage(){
+    cities = JSON.parse(localStorage.getItem("cities")) || [];
+}
+
+function saveToStorage() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+$(document).ready(function () {
+    loadFromStorage();
+    if (cities[0]) {
+        getCity(cities[cities.length - 1]);
+    }
+    citiesDisplay();
+
+
+    $(".btn").on("click", function (event) {
+        event.preventDefault();
+
+        var input = $(".form-control");
+        var city = input.val();
+        if (!cities.includes(city)) {
+            cities.push(city);
+            saveToStorage
+        }
+        citiesDisplay();
+        getCity(city);
+
+    });
+});
+
+
+//get city weather info
+function getCity(city) {
+    var currentDate = moment().format("LL");
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+// get API data with Ajax
+    $.ajax({ url: queryURL, type: "GET"}).then(function(response) {
+        console.log(response);
+
+        var iconLoc = response.weather[0].icon;
+        var iconSrc = "https://openweathermap.org/img/w/" + iconLoc + ".png";
+        var iconImage = $("<img>");
+        iconImage.attr("src", iconSrc);
+
+        $(".current-city").text(response.name + " (" + currentDate + ")");
+        $(".current-city").append(iconImage);
+        $("#temp").text("Temperature: " + response.main.temp + " F");
+        $("#hum").text("Humidity: " + response.main.humidity + " %");
+        $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
+
+        // TO DO - Need to convert the temperature but idk how
+        var tempFahr = (response.main.temp - 273.15) * 1.8 + 32;
+        $(".tempFahr").text("Temperature (Kelvin) " + tempFahr);
+        getUV(response.coord.lat, response.coord.lon)
+        forecast(city);
+        input.val("");
+        });
+}
+
+//display cities
+function citiesDisplay() {
+    var limit;
+
+    //limit display of cities to 10
+    if (cities.length < 10) {
+        limit = cities.length;
+    } else {
+        limit = 10;
+    }
+
+    $("#cityViewed").html("");
+    for (var i = 0; i < limit; i++) {
+        var cityViewed = $("<div>");
+        cityViewed.addClass("row").css({
+            textAlign: "center",
+            border: "1px solid white",
+            height: "50px",
+            lineHeight: "50px",
+            paddingLeft: "40px",
+        });
+        cityViewed.html(cities[i]);
+        $(`#${cities[i]}`).on("click", function () {
+            getCity($(this).text());
+        });
+    }
+}
+
+//maybe need get UV function here
+
+function getUV(lat, lon) {
+    var uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon + "&cnt=1";
+
+    $.ajax({ url: uvIndexURL, type: "GET" }).then(function (response) {
+        $("uv").text("UV-index : " + response[0].value);
+    });
+}
+
+//5 day forecast codes 
+function forecast(city) {
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+
+    $.ajax({ url: forecastURL, type: "GET"}).then(function (response) {
+        var list = response.list;
+        console.log(response);
+
+        $("#forecast").html("");
+        for (var i = 39; i >= 0; i = i - 8) {
+            var temp = ((list[i].main.temp - 273.15) * 1.8 + 32).toFixed(2);
+            var iconID = list[i].waether[0].icon;
+            var humidity = list[i].main.humidity;
+            var date = new Date(list[i].dt_txt);
+
+            var day = date.getDate();
+            var month = date.getMonth();
+            var year = date.getFullYear();
+
+            var newDate = `${month + 1}/${day}/${year}`;
+
+            //creating and storing a div tag
+            var col = $("<div>");
+            col.addClass("col");
+            var myCard = $("<div>");
+            myCard.addClass("card");
+            closed.append(myCard);
+
+            // creating a p tag with response item
+            var p = $("<p>").text(newDate);
+
+            // creating and storing an image tag
+            var iconURL = "https://openweathermap.org/img/wn" + iconID + "@2x.png";
+
+            var weatherImage = $("<img>");
+            weatherImage.attr("src", iconURL);
+
+            var p1 = $("<p>").text("Temp: " + temp + "F");
+            var p2 = $("<p>").text("Humidity: " + humidity + "%");
+
+
+            // appending the paragraph and image tags to myCard to show up 
+            myCard.append(p);
+            myCard.append(weatherImage);
+            myCard.append(p1);
+            myCard.append(p2);
+
+            $("#forecast").prepend(col);
+
+        }
+    });
+}
+ 
+
+
+//creating a p tag with the response item
+
+//appenign the paragraph and image to mycard 
+
